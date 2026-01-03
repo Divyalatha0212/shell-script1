91382360a740419b6833b69d407996b68e9460d7
Divyalatha0212 <divyalathaasadi@gmail.com>
2026-01-04 00:22:42 +0530

    variable script

--- 05-variable.sh
+++ 05-variable.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-echo "Please Enter the username::"
-read -s USERNAME
-echo "Please Enter the Password::"
-read -s PASSWORD
+#echo "Please Enter the username::"
+#read -s USERNAME
+#echo "Please Enter the Password::"
+#read -s PASSWORD
 
-echo "user name is: $USERNAME, password is: $PASSWORD"
+#echo "user name is: $USERNAME, password is: $PASSWORD"
+
+
+read -p "Please enter the username: " USERNAME
+read -s -p "Please enter the password: " PASSWORD
+
+echo
+echo "Username entered is: $USERNAME"
