b90ff89e374edf7a13c403c09b6e15826befe6e7
Divyalatha0212 <divyalathaasadi@gmail.com>
2026-01-04 00:02:40 +0530

    variable script

--- 04-variable.sh
+++ 04-variable.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-#declaring variable
 
 PERSON=$1
 PERSON=$2
 
-#refering variable
-
-#echo "$PERSON1:: Hello $PERSON2, How are you?"
-#echo "$PERSON2:: Hi $PERSON1, I am good, hw are you doing?"
-
-
-echo "$PERSON1:: Hello $PERSON2, How are you?"
-echo "$PERSON2:: Hi $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: Hi $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I ma Good."
