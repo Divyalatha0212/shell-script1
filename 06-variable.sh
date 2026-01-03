8492bfaaa3d746b459049991ead6bd9ecaf1e20e
Divyalatha0212 <divyalathaasadi@gmail.com>
2026-01-04 00:36:49 +0530

    variable script

--- 06-variable.sh
+++ 06-variable.sh
@@ -2,13 +2,13 @@
 
 read  -p "Please Enter user name:" USERNAME
 
-read -p -s "Please Enter password:" PASSWORD
+read -p -s "Please Enter password: " PASSWORD
 
 echo
-read -p -s "Confirm the Password:" CONFIRM_PASSWORD
+read -p -s "Confirm the Password: " CONFIRM_PASSWORD
 
-if [ PASSWORD==CONFIRM_PASSWORD]
+if [ "$PASSWORD" == "$CONFIRM_PASSWORD" ]; then
 echo "Password matched"
 else
-echo "Password not mached"
+echo "Password not mached" 
 fi
