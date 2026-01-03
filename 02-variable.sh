2332ddbadf02008ae962484d6ab1d4e3eae3ff2f
Divyalatha0212 <divyalathaasadi@gmail.com>
2026-01-03 21:45:14 +0530

    variable Script

---
+++ 02-variable.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Ramesh: : Hello Rajesh, How are you?"
+echo "Rajesh: : Hi Ramesh, I am good, How are you doing?"
+echo "Ramehs: : I am also doing fenstastic you konw one thinng I am Looking for job change now, if you come accress anything please let me know"
+echo "Rajesh: : Sure"
