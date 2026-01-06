5f09f196f4125d50d8bfd3c13c290345bcd60d39
Divyalatha0212 <divyalathaasadi@gmail.com>
2026-01-06 16:24:16 +0530

    special variables

---
+++ 09-special variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "all variables: $@"
+echo "Number of variables: $#"
+echo "Script Name: $0"
+echo "Current working directory: $WD"
+echo "Home Directory: $HOME"
+echo "Which user is running: $USER"
+echo "Host Name: $HOST"
+echo "Process ID of current shell script: $$"
