5d41778c818b3d7e99afdd603ea821f2fbfc6d66
Divyalatha0212 <divyalathaasadi@gmail.com>
2026-01-06 16:14:01 +0530

    datatypes

--- 08-datatypes.sh
+++ 08-datatypes.sh
@@ -3,4 +3,8 @@ NO1=$1
 NO2=$2
 
 SUM=$(($NO1 + $NO2))
+MUL=$(($NO1 * $NO2))
+DIF=$(($NO1 - $NO2))
 echo "total of $NO1 & $NO2 is: $SUM"
+echo "total of $NO1 & $NO2 is: $MUL"
+echo "total of $NO1 & $NO2 is: $DIF"
