ee17bb03d95d1f563bc0e3bf7deeacce44ad85dd
Divyalatha0212 <divyalathaasadi@gmail.com>
2026-01-06 16:56:44 +0530

    condition

---
+++ 11-installation.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+echo "Please run the script with root access"
+elase
+echo "you are the root user"
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]
+then
+echo "Installation is..FAILURE"
+exit 1
+else
+echo "Installing MYSQL..SUCCESS"
+fi
+
+dnf install git -y
+then 
+echo "Installing git is..FAILURE"
+exit 1
+else
+echo "Installing git is..SUCCESS"
+fi
