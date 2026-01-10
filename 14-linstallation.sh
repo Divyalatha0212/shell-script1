d9a74089bc8ddfb58d9384ba7ebd0d364ebcab77
Divyalatha0212 <divyalathaasadi@gmail.com>
2026-01-11 00:01:54 +0530

    loops

---
+++ 14-linstallation.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+USERID=$(id 0-u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo "$0 | cut -d "." -f1")
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $1 -ne 0 ]
+then
+echo -e "$2 $R FAILURE $N"
+else
+echo -e "$2 $G SUCCESS $N"
+fi
+
+
+if [ $USERID -ne 0 ]
+then
+echo " Run the script with root access "
+else
+echo " you are the root user "
+fi
+
+for i $@
+do 
+echo "package to install: $i"
+dnf list installed $i &>>LOGFILE
+if [ $? -eq 0 ]
+
+then
+echo -e $i already installed--$Y SKIPPING $N"
+
+else
+dnf install $i -y &>>LOGFILE
+fi
+done
+
