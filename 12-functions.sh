fac208d05fae2fee3236b06f38262a03721417b5
Divyalatha0212 <divyalathaasadi@gmail.com>
2026-01-10 23:28:30 +0530

    functions

---
+++ 12-functions.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%h-%m-%s)
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME$TIMESTAMP.log
+
+VALIDATE ()
+{
+    if [ $1 -ne 0 ]
+    then
+    echo " $2---FAILURE "
+    else
+    echo "$2---SUCCESS "
+    fi
+}
+
+
+if [ $USERDID -ne 0 ]
+then 
+echo " Please run the script in root"
+else
+echo " you are the root user."
+fi
+
+dnf install mysql -y
+VALIDATE &? "Installing mysql"
+
+dnf install git -y
+VALIDATE $? "installing git"
